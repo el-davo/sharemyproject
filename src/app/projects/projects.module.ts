9d5e84abeb793757b4016ff21aaf9487719e6d44
el-davo <david.ahern26@gmail.com>
2017-08-02 20:30:21 +0100

    Added datagrid

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ListComponent} from './list/list.component';
 import {ProjectsService} from './projects.service';
 import {ProjectsActions} from './projects.actions';
-import { ProjectsComponent } from './projects.component';
-import { CardComponent } from './card/card.component';
-import { AddProjectModalComponent } from './modal/add/add-project-modal.component';
+import {ProjectsComponent} from './projects.component';
+import {AddProjectModalComponent} from './modal/add/add-project-modal.component';
 import {ClrModalModule} from 'clarity-angular/modal/modal.module';
 import {NgReduxFormModule} from '@angular-redux/form/dist/source';
 import {ClrLoadingModule} from 'clarity-angular/utils/loading/loading.module'
+import {ClrDatagridModule} from 'clarity-angular/data/datagrid/datagrid.module';
 import {FormsModule} from '@angular/forms';
-import { DeleteProjectModalComponent } from './modal/delete/delete-project-modal.component';
+import {DeleteProjectModalComponent} from './modal/delete/delete-project-modal.component';
 
 @NgModule({
   imports: [
@@ -18,12 +18,12 @@ import { DeleteProjectModalComponent } from './modal/delete/delete-project-modal
     ClrModalModule,
     NgReduxFormModule,
     FormsModule,
-    ClrLoadingModule
+    ClrLoadingModule,
+    ClrDatagridModule
   ],
   declarations: [
     ListComponent,
     ProjectsComponent,
-    CardComponent,
     AddProjectModalComponent,
     DeleteProjectModalComponent
   ],
@@ -35,4 +35,5 @@ import { DeleteProjectModalComponent } from './modal/delete/delete-project-modal
     AddProjectModalComponent
   ]
 })
-export class ProjectsModule { }
+export class ProjectsModule {
+}
