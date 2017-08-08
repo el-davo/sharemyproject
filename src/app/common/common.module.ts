bc2a4d3e2c7b42edf1081ee5cad68b33fb31e2c5
el-davo <david.ahern26@gmail.com>
2017-08-08 08:22:05 +0100

    Adding right icon button

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {MdListComponent} from './material/md-list/md-list.component';
 import {MdListItemComponent} from './material/md-list-item/md-list-item.component';
+import { MdListRightIconButtonComponent } from './material/md-list-right-icon-button/md-list-right-icon-button.component';
 
 @NgModule({
   declarations: [
     MdListComponent,
-    MdListItemComponent
+    MdListItemComponent,
+    MdListRightIconButtonComponent
   ],
   exports: [
     MdListComponent,
