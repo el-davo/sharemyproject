1bb2ae9cdaa469779646d2b02f894f9ddfbe1977
el-davo <david.ahern26@gmail.com>
2017-08-08 20:52:15 +0100

    Adding spinners

--- src/app/lists/list.module.ts
+++ src/app/lists/list.module.ts
@@ -6,7 +6,6 @@ import {ListActions} from './list.actions';
 import {RouterModule} from '@angular/router';
 import {ClrIconModule} from 'clarity-angular/icon/icon.module';
 import {ClrModalModule} from 'clarity-angular/modal/modal.module';
-import {ClrDatagridModule} from 'clarity-angular/data/datagrid/datagrid.module';
 import {NgReduxFormModule} from '@angular-redux/form/dist/source';
 import {ClrLoadingModule} from 'clarity-angular/utils/loading/loading.module'
 import {FormsModule} from '@angular/forms';
@@ -15,6 +14,7 @@ import {CardComponent} from './card/card.component';
 import {AddListModalComponent} from './modal/add/add-list-modal.component';
 import {DeleteListModalComponent} from './modal/delete/delete-list-modal.component';
 import {AddLinkModalComponent} from './modal/add-link/add-link-modal.component';
+import {AppCommonModule} from '../common/common.module';
 
 @NgModule({
   imports: [
@@ -25,7 +25,7 @@ import {AddLinkModalComponent} from './modal/add-link/add-link-modal.component';
     NgReduxFormModule,
     ClrLoadingModule,
     FormsModule,
-    ClrDatagridModule
+    AppCommonModule
   ],
   declarations: [
     ListComponent,
