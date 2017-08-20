7f9621e20e4ccde1d168670034d6785426441c45
el-davo <david.ahern26@gmail.com>
2017-08-20 19:23:10 +0100

    Added animation to hover button

--- src/app/feedback/hover-button/hover-button.component.ts
+++ src/app/feedback/hover-button/hover-button.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
 import {FeedbackActions} from '../feedback.actions';
 import {dispatch, select} from '@angular-redux/store';
 import {Observable} from 'rxjs/Observable';
@@ -12,11 +12,22 @@ export class HoverButtonComponent {
 
   @select(['feedback', 'showFeedbackHoverButton']) showFeedbackHoverButton$: Observable<boolean>;
 
-  constructor(private feedbackAction: FeedbackActions) {
+  constructor(private elementRef: ElementRef, private feedbackAction: FeedbackActions) {
   }
 
   @dispatch()
   showFeedbackModal() {
     return this.feedbackAction.showFeedbackModal();
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    console.log(this.elementRef.nativeElement);
+    this.elementRef.nativeElement.className = 'animated infinite pulse';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.elementRef.nativeElement.className = '';
   }
 }
