420b05e899a6308f147614a097470755d19b8565
ahernd2 <david.ahern@emc.com>
2017-08-28 20:12:43 +0100

    Added landing and token components and modules

--- src/app/screenshots/screenshots.module.ts
+++ src/app/screenshots/screenshots.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TokensModule} from './tokens/tokens.module';
+import {LandingModule} from './landing/landing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TokensModule,
+    LandingModule
   ],
   declarations: []
 })
-export class ScreenshotsModule { }
+export class ScreenshotsModule {
+}
---
+++ src/app/screenshots/tokens/tokens.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TokensComponent } from './tokens.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TokensComponent]
+})
+export class TokensModule { }
