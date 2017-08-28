d9058ec07d97976bad1f758026958dcac2b9a97c
ahernd2 <david.ahern@emc.com>
2017-08-28 20:42:04 +0100

    Adding s3 dashboard

---
+++ src/app/screenshots/s3/s3.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { S3Component } from './s3.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [S3Component]
+})
+export class S3Module { }
